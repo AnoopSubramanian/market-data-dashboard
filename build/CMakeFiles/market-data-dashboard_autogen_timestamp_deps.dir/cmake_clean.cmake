08f27d90f7a5880841bc0464e78c95aff7f46a2e
Anoop Subramanian <anoop.mech.iitkgp@gmail.com>
2025-06-09 09:09:33 +0200

    remove build folder

--- build/CMakeFiles/market-data-dashboard_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/market-data-dashboard_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/market-data-dashboard_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
